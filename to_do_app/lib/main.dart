5ba484188e9109f32c62993180e41a7a1808c1c6
Your Name <your_email@example.com>
2025-01-22 18:26:25 +0200

    Remove commented-out code and integrate Bloc observer in main app entry point

--- to_do_app/lib/main.dart
+++ to_do_app/lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:to_do_app/core/utils/bloc_observer.dart';
 import 'package:to_do_app/features/welcome/presentation/view/welcome_screen.dart';
 
 void main() {
+  Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
