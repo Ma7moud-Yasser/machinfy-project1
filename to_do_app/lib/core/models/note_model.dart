36c1c6727b3c1affd9c913180d62aa96b461e4bb
Your Name <your_email@example.com>
2025-01-22 23:49:08 +0200

    Add NoteModel class and implement stateful NoteScreen with note listing and Lottie animations

---
+++ to_do_app/lib/core/models/note_model.dart
@@ -0,0 +1,11 @@
+class NoteModel {
+  String title;
+  String description;
+  String date;
+
+  NoteModel({
+    required this.title,
+    required this.description,
+    required this.date,
+  });
+}
--- to_do_app/lib/features/note/presentation/view/note_screen.dart
+++ to_do_app/lib/features/note/presentation/view/note_screen.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
+import 'package:to_do_app/core/styles/assets_manager.dart';
 import 'package:to_do_app/core/utils/responsive_font.dart';
 import 'package:to_do_app/features/note/presentation/component/custom_search_bar.dart';
 import '../controller/note_screen_cubit.dart';
 import '../controller/note_screen_states.dart';
 
-class NoteScreen extends StatelessWidget {
+class NoteScreen extends StatefulWidget {
   const NoteScreen({super.key});
   static const route = '/noteScreen';
 
+  @override
+  State<NoteScreen> createState() => _NoteScreenState();
+}
+
+class _NoteScreenState extends State<NoteScreen> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -41,7 +48,81 @@ class NoteScreen extends StatelessWidget {
                               )),
                     ),
                   ),
-                  CustomSearchBar()
+                  CustomSearchBar(),
+                  // SizedBox(
+                  //   height: MediaQuery.sizeOf(context).height * .01,
+                  // ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: 5,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: MediaQuery.sizeOf(context).width * .01,
+                            vertical: MediaQuery.sizeOf(context).height * .01,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(
+                                MediaQuery.sizeOf(context).height * 0.02),
+                            color: Theme.of(context)
+                                .colorScheme
+                                .primary
+                                .withOpacity(0.2),
+                          ),
+                          child: ListTile(
+                            title: Text(
+                              "First Note",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyLarge!
+                                  .copyWith(
+                                    color:
+                                        Theme.of(context).colorScheme.tertiary,
+                                  ),
+                            ),
+                            subtitle: Text(
+                              "their is note their is note their is note",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .copyWith(
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .tertiary
+                                        .withOpacity(0.6),
+                                  ),
+                            ),
+                            trailing: GestureDetector(
+                              onTap: () async {
+                                Lottie.asset(
+                                  IconsAssets.trash,
+                                  width:
+                                      MediaQuery.sizeOf(context).width * 0.15,
+                                  height:
+                                      MediaQuery.sizeOf(context).height * 0.15,
+                                  repeat: false,
+                                  animate: true,
+                                );
+
+                                await Future.delayed(
+                                    const Duration(seconds: 1));
+
+                                setState(() {
+                                  // notes.removeAt(index);
+                                });
+                              },
+                              child: Lottie.asset(
+                                IconsAssets.trash,
+                                width: MediaQuery.sizeOf(context).width * 0.15,
+                                height:
+                                    MediaQuery.sizeOf(context).height * 0.15,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -51,3 +132,14 @@ class NoteScreen extends StatelessWidget {
     );
   }
 }
+
+// class NotesCard extends StatelessWidget {
+//   const NotesCard({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ;
+//   }
+// }
