7c193cf8b7898031d6f2d2db9b092f7cd30a95f6
Your Name <your_email@example.com>
2025-02-04 16:40:05 +0200

    Enhance customSnackBar styling with bold text and themed colors for improved visibility

--- to_do_app/lib/core/components/custom_snackBar.dart
+++ to_do_app/lib/core/components/custom_snackBar.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/core/styles/color_manager.dart';
 
 ScaffoldFeatureController<SnackBar, SnackBarClosedReason> customSnackBar(
     BuildContext context,
     {required String message}) {
   return ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(content: Text(message)),
+    SnackBar(
+      content: Text(
+        message,
+        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+              color: Theme.of(context).colorScheme.secondary,
+              fontWeight: FontWeight.bold,
+            ),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.primary,
+    ),
   );
 }
