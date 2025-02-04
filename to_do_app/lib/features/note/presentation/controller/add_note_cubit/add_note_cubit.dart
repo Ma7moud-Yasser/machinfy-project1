8c3146dcb46cd826ad704c4e592489097eeca1c2
Your Name <your_email@example.com>
2025-02-04 14:29:55 +0200

    Implement addNotes method in AddNoteCubit to handle note addition with Hive

--- to_do_app/lib/features/note/presentation/controller/add_note_cubit/add_note_cubit.dart
+++ to_do_app/lib/features/note/presentation/controller/add_note_cubit/add_note_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:to_do_app/core/models/note_model.dart';
+import 'package:to_do_app/core/resources/app_stings.dart';
 import 'add_note_states.dart';
 
 class AddNoteCubit extends Cubit<AddNoteStates> {
@@ -12,6 +14,16 @@ class AddNoteCubit extends Cubit<AddNoteStates> {
   AddNoteCubit() : super(AddNoteInitState());
 
   static AddNoteCubit get(context) => BlocProvider.of(context);
+  addNotes(NoteModel note) async {
+    emit(AddNoteLoadingState());
+    try {
+      var notesBox = Hive.box<NoteModel>(AppString.notesBox);
+      await notesBox.add(note);
+      emit(AddNoteSuccessState());
+    } on Exception catch (e) {
+      emit(AddNoteErrorState(e.toString()));
+    }
+  }
 
   void addNote(List<NoteModel> notes, BuildContext context) {
     try {
