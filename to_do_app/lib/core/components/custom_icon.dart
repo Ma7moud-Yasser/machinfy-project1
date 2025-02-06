b820d5e30a2e5437aa9fc857ccdfbab62acf144e
Your Name <your_email@example.com>
2025-02-06 13:33:57 +0200

    Add CustomIcon widget for reusable icon component with styling

---
+++ to_do_app/lib/core/components/custom_icon.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({
+    super.key,
+    required this.icon,
+  });
+  final IconData? icon;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.all(MediaQuery.sizeOf(context).width * 0.03),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).colorScheme.primary,
+              blurRadius: 6,
+              offset: Offset(0, 0),
+            ),
+          ],
+          borderRadius:
+              BorderRadius.circular(MediaQuery.sizeOf(context).width * 0.04),
+          color: Theme.of(context).colorScheme.primary.withOpacity(0.8),
+        ),
+        child: Icon(
+          icon,
+          color: Theme.of(context).colorScheme.secondary,
+          size: MediaQuery.sizeOf(context).width * 0.09,
+        ),
+      ),
+    );
+  }
+}
