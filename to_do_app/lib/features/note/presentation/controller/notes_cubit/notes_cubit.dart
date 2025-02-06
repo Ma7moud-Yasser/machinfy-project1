5ec8fea4ebc96db284dbfc1077d5df69303e0755
Your Name <your_email@example.com>
2025-02-06 08:58:29 +0200

    Refactor NotesCubit to remove hardcoded notes list and change NotesState to an abstract class

--- to_do_app/lib/features/note/presentation/controller/notes_cubit/notes_cubit.dart
+++ to_do_app/lib/features/note/presentation/controller/notes_cubit/notes_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:meta/meta.dart';
 import 'package:to_do_app/core/models/note_model.dart';
 import 'package:to_do_app/core/resources/app_stings.dart';
 
@@ -8,33 +7,7 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
-  List<NoteModel>? notes = [
-    NoteModel(
-      title: "First Note",
-      description: "their is note their is note their is note",
-      date: "10/1/2025",
-    ),
-    NoteModel(
-      title: "Second Note",
-      description: "their is note their is note their is note",
-      date: "1/1/2025",
-    ),
-    NoteModel(
-      title: "Third Note",
-      description: "their is note their is note their is note",
-      date: "12/12/2024",
-    ),
-    NoteModel(
-      title: "Fourth Note",
-      description: "their is note their is note their is note",
-      date: "8/12/2024",
-    ),
-    NoteModel(
-      title: "Fifth Note",
-      description: "their is note their is note their is note",
-      date: "1/12/2024",
-    ),
-  ];
+  List<NoteModel>? notes;
 
   fetchAllNotes() {
     // emit(NotesLoading());
