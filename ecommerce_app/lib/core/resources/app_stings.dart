a298c6513280b990f991d75ced149bbc5164429b
Your Name <your_email@example.com>
2025-02-18 15:41:11 +0200

    Add localized strings for login, email, password, and related prompts

--- ecommerce_app/lib/core/resources/app_stings.dart
+++ ecommerce_app/lib/core/resources/app_stings.dart
@@ -2,5 +2,12 @@ class AppString {
   static String appName = 'Jawda';
   static String appSubName = 'Find. Click. Smile';
   static String loginTitle = 'You\'re one step closer to start';
+  static String login = 'Log in';
   static String signUpTitle = 'You\'re one step closer to start';
+  static String email = 'E-mail';
+  static String exEmail = 'ex: example@example.com';
+  static String password = 'Password';
+  static String exPassword = 'ex: ***************************';
+  static String rememberMe = 'Remember me';
+  static String forgetPassword = 'Forget password?';
 }
