90aecfdcddd06cbcb91b58897aca22dbe35aa4e9
Your Name <your_email@example.com>
2025-02-18 13:51:14 +0200

    Refactor color constants in AppColor for improved clarity and organization

--- ecommerce_app/lib/core/styles/color_manager.dart
+++ ecommerce_app/lib/core/styles/color_manager.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static const primary = Color(0xff105D5E);
-  static const black = Color(0xff293E33);
-  static const secondary = Color(0xffE1E7E0);
-  static const orange = Color(0xffE85613);
+  static const tertiary = Color(0xff293E33);
+  static const background = Color(0xffE1E7E0);
+  static const secondary = Color(0xffE85613);
   static const gray = Color(0xff767F7D);
   static const success = Color(0xff22A901);
   static const firebrick = Color(0xffB01717);
