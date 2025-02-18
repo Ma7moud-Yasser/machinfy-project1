b43514d824c7831001d2aeac1d1b7e4ab0f7610f
Your Name <your_email@example.com>
2025-02-18 21:12:55 +0200

    Refactor LoginScreenCubit to improve state management and add rememberMe functionality

--- ecommerce_app/lib/features/login/presentation/controller/login_screen_cubit.dart
+++ ecommerce_app/lib/features/login/presentation/controller/login_screen_cubit.dart
@@ -1,16 +1,21 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'login_screen_states.dart';
 
 class LoginScreenCubit extends Cubit<LoginScreenStates> {
-  bool _isActive = true;
-
+  bool isActive = true;
+  bool rememberMe = false;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   LoginScreenCubit() : super(LoginScreenInitState());
 
   static LoginScreenCubit get(context) => BlocProvider.of(context);
 
+  void login() {}
+
   @override
   Future<void> close() {
-    _isActive = false;
+    isActive = false;
     return super.close();
   }
 }
