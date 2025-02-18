b477a7b94a63bbdb72d9af15b38d2e915e5f3bfd
Your Name <your_email@example.com>
2025-02-18 13:51:02 +0200

    Add login and sign-up titles to AppString for improved user guidance

--- ecommerce_app/lib/core/resources/app_stings.dart
+++ ecommerce_app/lib/core/resources/app_stings.dart
@@ -1,4 +1,6 @@
 class AppString {
   static String appName = 'Jawda';
   static String appSubName = 'Find. Click. Smile';
+  static String loginTitle = 'You\'re one step closer to start';
+  static String signUpTitle = 'You\'re one step closer to start';
 }
