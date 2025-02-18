6c3f2dae866d69b52924edbed68ab2301cd578fa
Your Name <your_email@example.com>
2025-02-18 16:29:38 +0200

    Refactor RememberAndForgetPassword component to use padding manager for consistent layout

--- ecommerce_app/lib/features/login/presentation/component/remember_forget_password.dart
+++ ecommerce_app/lib/features/login/presentation/component/remember_forget_password.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/core/resources/app_stings.dart';
+import 'package:ecommerce_app/core/styles/padding_manager.dart';
 import 'package:ecommerce_app/core/styles/styles_manager.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,39 +9,44 @@ class RememberAndForgetPassword extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
-          spacing: 10,
-          children: [
-            SizedBox(
-              width: MediaQuery.sizeOf(context).width * .05,
-              child: Checkbox(
-                side: BorderSide(color: Theme.of(context).colorScheme.outline),
-                value: false,
-                onChanged: (value) {
-                  ///
-                },
+    return Padding(
+      padding: RememberAndForgetPasswordPadding.getPadding(context),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            spacing: 10,
+            children: [
+              SizedBox(
+                width: MediaQuery.sizeOf(context).width * .05,
+                child: Checkbox(
+                  side: BorderSide(
+                    color: Theme.of(context).colorScheme.outline,
+                  ),
+                  value: false,
+                  onChanged: (value) {
+                    ///
+                  },
+                ),
               ),
-            ),
-            Text(
-              AppString.rememberMe,
-              style: StyleManager.textStyle14(context).copyWith(
-                fontWeight: FontWeight.w600,
-                color: Theme.of(context).colorScheme.primary,
+              Text(
+                AppString.rememberMe,
+                style: StyleManager.textStyle14(context).copyWith(
+                  fontWeight: FontWeight.w600,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
               ),
-            ),
-          ],
-        ),
-        Text(
-          AppString.forgetPassword,
-          style: StyleManager.textStyle14(context).copyWith(
-            fontWeight: FontWeight.w600,
-            color: Theme.of(context).colorScheme.primary,
+            ],
           ),
-        ),
-      ],
+          Text(
+            AppString.forgetPassword,
+            style: StyleManager.textStyle14(context).copyWith(
+              fontWeight: FontWeight.w600,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
