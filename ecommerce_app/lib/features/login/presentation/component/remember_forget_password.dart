eca94e708dcdb6ccfb85ffb525a0aaa3abb8a3a8
Your Name <your_email@example.com>
2025-02-18 15:41:32 +0200

    Add RememberAndForgetPassword component for login screen

---
+++ ecommerce_app/lib/features/login/presentation/component/remember_forget_password.dart
@@ -0,0 +1,46 @@
+import 'package:ecommerce_app/core/resources/app_stings.dart';
+import 'package:ecommerce_app/core/styles/styles_manager.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RememberAndForgetPassword extends StatelessWidget {
+  const RememberAndForgetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          spacing: 10,
+          children: [
+            SizedBox(
+              width: MediaQuery.sizeOf(context).width * .05,
+              child: Checkbox(
+                side: BorderSide(color: Theme.of(context).colorScheme.outline),
+                value: false,
+                onChanged: (value) {
+                  ///
+                },
+              ),
+            ),
+            Text(
+              AppString.rememberMe,
+              style: StyleManager.textStyle14(context).copyWith(
+                fontWeight: FontWeight.w600,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+          ],
+        ),
+        Text(
+          AppString.forgetPassword,
+          style: StyleManager.textStyle14(context).copyWith(
+            fontWeight: FontWeight.w600,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
+      ],
+    );
+  }
+}
