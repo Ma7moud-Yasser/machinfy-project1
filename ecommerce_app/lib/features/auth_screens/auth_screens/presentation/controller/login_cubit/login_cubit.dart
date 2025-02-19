ffbc569154f08a6126993479b24de68fba31b20f
Your Name <your_email@example.com>
2025-02-19 20:10:41 +0200

    Add rememberMe field to LoginCubit for user session management

--- ecommerce_app/lib/features/auth_screens/auth_screens/presentation/controller/login_cubit/login_cubit.dart
+++ ecommerce_app/lib/features/auth_screens/auth_screens/presentation/controller/login_cubit/login_cubit.dart
@@ -7,6 +7,7 @@ part 'login_state.dart';
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitial());
   static LoginCubit get(context) => BlocProvider.of(context);
+  bool rememberMe = false;
 
   void login() {}
 }
