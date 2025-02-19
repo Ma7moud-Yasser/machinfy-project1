a4f9abe36690621196d9bf94877df5da608414ee
Your Name <your_email@example.com>
2025-02-19 19:48:31 +0200

    Refactor route manager to consolidate authentication screens and remove unused routes

--- ecommerce_app/lib/core/resources/route_manager.dart
+++ ecommerce_app/lib/core/resources/route_manager.dart
@@ -1,6 +1,5 @@
-import 'package:ecommerce_app/features/login/presentation/view/login_screen.dart';
 import 'package:ecommerce_app/features/on_boardings/presentation/view/on_boarding_screens.dart';
-import 'package:ecommerce_app/features/sign_up/presentation/view/sign_up_screen.dart';
+import 'package:ecommerce_app/features/auth_screens/auth_screens/presentation/view/auth_screens.dart';
 import 'package:ecommerce_app/features/splash/presentation/view/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -8,8 +7,8 @@ class Routes {
   static const String introRoute = "/";
   static const String splashScreen = "/splashScreen";
   static const String onBoardingScreen = "/onBoardingScreen";
-  static const String loginScreen = "/loginScreen";
-  static const String signUpScreen = "/signUpScreen";
+  // static const String loginScreen = "/loginScreen";
+  static const String authScreens = "/authScreens";
 }
 
 class RouteGenerator {
@@ -17,12 +16,12 @@ class RouteGenerator {
     switch (routeSettings.name) {
       case Routes.splashScreen:
         return MaterialPageRoute(builder: (_) => const SplashScreen());
-      case Routes.signUpScreen:
-        return MaterialPageRoute(builder: (_) => const SignUpScreen());
+      case Routes.authScreens:
+        return MaterialPageRoute(builder: (_) => const AuthScreens());
       case Routes.onBoardingScreen:
         return MaterialPageRoute(builder: (_) => const OnboardingScreen());
-      case Routes.loginScreen:
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      // case Routes.loginScreen:
+      // return MaterialPageRoute(builder: (_) => const LoginScreen());
       default:
         return unDefinedRoute();
     }
