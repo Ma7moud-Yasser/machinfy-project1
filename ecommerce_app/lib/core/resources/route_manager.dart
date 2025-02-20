be3b28369238be48a9565a69f32b74bed6aede63
Your Name <your_email@example.com>
2025-02-20 19:17:46 +0200

    Add route for forget password screen in route manager

--- ecommerce_app/lib/core/resources/route_manager.dart
+++ ecommerce_app/lib/core/resources/route_manager.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/features/auth_screens/forget_password/presentation/view/forget_password_screen.dart';
 import 'package:ecommerce_app/features/on_boardings/presentation/view/on_boarding_screens.dart';
 import 'package:ecommerce_app/features/auth_screens/auth_screens/presentation/view/auth_screens.dart';
 import 'package:ecommerce_app/features/splash/presentation/view/splash_screen.dart';
@@ -9,6 +10,7 @@ class Routes {
   static const String onBoardingScreen = "/onBoardingScreen";
   // static const String loginScreen = "/loginScreen";
   static const String authScreens = "/authScreens";
+  static const String forgetPasswordScreens = "/forgetPasswordScreens";
 }
 
 class RouteGenerator {
@@ -20,6 +22,8 @@ class RouteGenerator {
         return MaterialPageRoute(builder: (_) => const AuthScreens());
       case Routes.onBoardingScreen:
         return MaterialPageRoute(builder: (_) => const OnboardingScreen());
+      case Routes.forgetPasswordScreens:
+        return MaterialPageRoute(builder: (_) => const ForgetPasswordScreen());
       // case Routes.loginScreen:
       // return MaterialPageRoute(builder: (_) => const LoginScreen());
       default:
