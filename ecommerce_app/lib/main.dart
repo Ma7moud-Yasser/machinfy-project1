b5acf21cb91c6f09f4519ee1ccd7bfa527b92376
Your Name <your_email@example.com>
2025-02-20 17:20:05 +0200

    Comment out home route in MyApp for future implementation

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -38,6 +38,7 @@ class MyApp extends StatelessWidget {
           themeMode: mode,
           initialRoute: Routes.splashScreen,
           onGenerateRoute: RouteGenerator.getRoute,
+          // home: MyHomePage(),
         );
       },
     );
