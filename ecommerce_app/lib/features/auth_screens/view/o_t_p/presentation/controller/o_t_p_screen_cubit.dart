f07f05b421cfa79d665ff422acc2792d95073920
Your Name <your_email@example.com>
2025-02-24 18:17:45 +0200

    Implement OTP screen with input fields, resend code functionality, and navigation to reset password

--- ecommerce_app/lib/features/auth_screens/view/o_t_p/presentation/controller/o_t_p_screen_cubit.dart
+++ ecommerce_app/lib/features/auth_screens/view/o_t_p/presentation/controller/o_t_p_screen_cubit.dart
@@ -1,16 +1,25 @@
+import 'dart:developer';
+
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'o_t_p_screen_states.dart';
 
 class OTPScreenCubit extends Cubit<OTPScreenStates> {
-  bool _isActive = true;
-
   OTPScreenCubit() : super(OTPScreenInitState());
-
+  bool isActive = true;
   static OTPScreenCubit get(context) => BlocProvider.of(context);
+  List<TextEditingController> otpController = List.generate(
+    4,
+    (index) => TextEditingController(),
+  );
+
+  resendCode() {
+    log('ReSending Code....');
+  }
 
   @override
   Future<void> close() {
-    _isActive = false;
+    isActive = false;
     return super.close();
   }
 }
--- ecommerce_app/lib/features/auth_screens/view/o_t_p/presentation/view/o_t_p_screen.dart
+++ ecommerce_app/lib/features/auth_screens/view/o_t_p/presentation/view/o_t_p_screen.dart
@@ -1,5 +1,17 @@
+import 'dart:developer';
+
+import 'package:ecommerce_app/core/components/custom_elevated_button.dart';
+import 'package:ecommerce_app/core/resources/app_stings.dart';
+import 'package:ecommerce_app/core/resources/route_manager.dart';
+import 'package:ecommerce_app/core/styles/assets_manager.dart';
+import 'package:ecommerce_app/core/styles/border_radius_manager.dart';
+import 'package:ecommerce_app/core/styles/padding_manager.dart';
+import 'package:ecommerce_app/core/styles/size_manager.dart';
+import 'package:ecommerce_app/core/styles/styles_manager.dart';
+import 'package:ecommerce_app/features/auth_screens/components/auth_title.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../controller/o_t_p_screen_cubit.dart';
 import '../controller/o_t_p_screen_states.dart';
 
@@ -14,7 +26,146 @@ class OTPScreen extends StatelessWidget {
         listener: (context, state) {},
         builder: (context, state) {
           final cubit = OTPScreenCubit.get(context);
-          return Scaffold();
+          return Scaffold(
+            body: SafeArea(
+              child: Padding(
+                padding: PaddingManager.main(context),
+                child: SingleChildScrollView(
+                  child: Form(
+                    // key: cubit.formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      spacing: SizeManager.getSize(context).height * 0.02,
+                      children: [
+                        Stack(
+                          children: [
+                            AuthTitleHeader(
+                              imagePath: IconsAssets.forgetPassword,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                top: SizeManager.getSize(context).height * 0.02,
+                              ),
+                              child: IconButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                icon: SvgPicture.asset(IconsAssets.arrowBack),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          textAlign: TextAlign.center,
+                          AppString.verificationCodeTitle,
+                          style: StyleManager.textStyle16(context),
+                        ),
+                        Text(
+                          textAlign: TextAlign.center,
+                          AppString.enterVerificationCode,
+                          style: StyleManager.textStyle22(
+                            context,
+                          ).copyWith(fontWeight: FontWeight.bold),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          spacing: 10,
+                          children: List.generate(
+                            4,
+                            (index) => SizedBox(
+                              width: SizeManager.getSize(context).width * 0.12,
+                              height:
+                                  SizeManager.getSize(context).height * 0.06,
+                              child: TextFormField(
+                                controller: cubit.otpController[index],
+                                textAlign: TextAlign.center,
+                                keyboardType: TextInputType.number,
+                                style: StyleManager.textStyle16(
+                                  context,
+                                ).copyWith(
+                                  color: Theme.of(context).colorScheme.primary,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                onChanged: (value) {
+                                  if (value.isNotEmpty) {
+                                    if (index < 3) {
+                                      FocusScope.of(context).nextFocus();
+                                    } else {
+                                      FocusScope.of(context).unfocus();
+                                    }
+                                  } else if (index > 0) {
+                                    FocusScope.of(context).previousFocus();
+                                  }
+                                },
+                                decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor:
+                                      Theme.of(context).colorScheme.background,
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadiusManager.small(
+                                      context,
+                                    ),
+                                    borderSide: BorderSide(
+                                      color:
+                                          Theme.of(
+                                            context,
+                                          ).colorScheme.background,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadiusManager.small(
+                                      context,
+                                    ),
+                                    borderSide: BorderSide(
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                      width: 2,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          AppString.dontReceiveCode,
+                          style: StyleManager.textStyle12(context),
+                        ),
+                        GestureDetector(
+                          onTap: () => cubit.resendCode(),
+                          child: Text(
+                            AppString.resendCode,
+                            style: StyleManager.textStyle14(context).copyWith(
+                              color: Theme.of(context).colorScheme.secondary,
+                            ),
+                          ),
+                        ),
+                        CustomElevatedButton(
+                          onPressed: () {
+                            if (cubit.otpController.length < 4 ||
+                                cubit.otpController.any(
+                                  (element) => element.text.isEmpty,
+                                )) {
+                              log('Please enter the correct code');
+                            } else {
+                              Navigator.pushNamed(
+                                context,
+                                Routes.resetPasswordScreen,
+                              );
+                            }
+                            // if (cubit.formKey.currentState!.validate()) {
+                            //   Navigator.pushNamed(context, Routes.otpScreen);
+                            // }
+                          },
+                          textButton: AppString.confirm,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
         },
       ),
     );
