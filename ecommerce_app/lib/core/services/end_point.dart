9578379c64c540af299b73779067b8851d07ddbd
Your Name <your_email@example.com>
2025-03-11 23:39:00 +0200

    Update API base URL and add additional endpoints in EndPoint class; modify .gitignore to exclude Postman collection and temporary files

--- ecommerce_app/lib/core/services/end_point.dart
+++ ecommerce_app/lib/core/services/end_point.dart
@@ -1,5 +1,9 @@
 class EndPoint {
-  static const String baseUrl = 'https://student.valuxapps.com/api/';
+  static const String baseUrl = 'https://store.mediagrafico.com/api/';
   static const String login = 'login';
   static const String register = 'register';
+  static const String forgotPassword = 'forget-password';
+  static const String verifyAccount = 'verify-account';
+  static const String sendEmail = 'send-email';
+  static const String suspendAccount = 'suspend-account';
 }
