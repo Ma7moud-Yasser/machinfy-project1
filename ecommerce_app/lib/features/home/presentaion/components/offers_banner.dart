a022a6982a5f752b819e09f59c3795d0b273bfee
Your Name <your_email@example.com>
2025-03-14 14:21:50 +0200

    Refactor OffersBanner to accept slider items as a parameter; update home screen to pass slider items for dynamic content

--- ecommerce_app/lib/features/home/presentaion/components/offers_banner.dart
+++ ecommerce_app/lib/features/home/presentaion/components/offers_banner.dart
@@ -1,14 +1,17 @@
+import 'dart:developer';
+
 import 'package:carousel_slider/carousel_slider.dart';
 
 import 'package:ecommerce_app/core/styles/color_manager.dart';
 import 'package:ecommerce_app/features/home/presentaion/components/buid_card_banner.dart';
-import 'package:ecommerce_app/features/home/presentaion/components/details_item_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OffersBanner extends StatefulWidget {
-  const OffersBanner({super.key});
+  const OffersBanner({super.key, required this.sliderItems});
+
+  final List<Map<String, dynamic>> sliderItems;
 
   @override
   State<OffersBanner> createState() => _OffersBannerState();
@@ -17,30 +20,16 @@ class OffersBanner extends StatefulWidget {
 class _OffersBannerState extends State<OffersBanner> {
   final CarouselSliderController _carouselController =
       CarouselSliderController();
+  bool autoPlay = true;
   int _currentIndex = 0;
-  final List<Map<String, dynamic>> sliderItems = const [
-    {
-      "image":
-          "https://i.pinimg.com/736x/27/8e/46/278e46ef553cc3176963ed0d52077cdd.jpg",
-      "title": "Elegant Black Watch",
-      "subtitle": "Discover timeless elegance.",
-      "page": DetailPage(title: "Page 1"),
-    },
-    {
-      "image":
-          "https://i.pinimg.com/736x/73/8c/66/738c66a1e117f638020803ff8c6a6575.jpg",
-      "title": "Luxury Silver Watch",
-      "subtitle": "Refined style for modern times.",
-      "page": DetailPage(title: "Page 2"),
-    },
-    {
-      "image":
-          "https://i.pinimg.com/736x/f1/ca/4a/f1ca4a2b4143a4ce875b20261bc91a4c.jpg",
-      "title": "Classic Leather Watch",
-      "subtitle": "Vintage charm meets innovation.",
-      "page": DetailPage(title: "Page 3"),
-    },
-  ];
+
+  @override
+  dispose() {
+    _carouselController.stopAutoPlay();
+    autoPlay = false;
+    log("autoPlay: $autoPlay");
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +39,7 @@ class _OffersBannerState extends State<OffersBanner> {
           carouselController: _carouselController,
           options: CarouselOptions(
             height: 205,
-            autoPlay: true,
+            autoPlay: autoPlay,
             enlargeCenterPage: true,
             viewportFraction: 1.0,
             onPageChanged: (index, reason) {
@@ -60,7 +49,7 @@ class _OffersBannerState extends State<OffersBanner> {
             },
           ),
           items:
-              sliderItems.map((item) {
+              widget.sliderItems.map((item) {
                 return GestureDetector(
                   onTap: () {
                     Navigator.push(
@@ -80,7 +69,7 @@ class _OffersBannerState extends State<OffersBanner> {
         Center(
           child: AnimatedSmoothIndicator(
             activeIndex: _currentIndex,
-            count: sliderItems.length,
+            count: widget.sliderItems.length,
             effect: ExpandingDotsEffect(
               dotHeight: 8,
               dotWidth: 8,
--- ecommerce_app/lib/features/home/presentaion/views/home_screen.dart
+++ ecommerce_app/lib/features/home/presentaion/views/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:ecommerce_app/core/styles/size_manager.dart';
 import 'package:ecommerce_app/core/styles/styles_manager.dart';
 import 'package:ecommerce_app/features/home/presentaion/components/custom_search_bar.dart';
 import 'package:ecommerce_app/features/home/presentaion/components/custom_user_bar.dart';
+import 'package:ecommerce_app/features/home/presentaion/components/details_item_screen.dart';
 import 'package:ecommerce_app/features/home/presentaion/components/offers_banner.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +16,29 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<Map<String, dynamic>> sliderItems = const [
+    {
+      "image":
+          "https://i.pinimg.com/736x/27/8e/46/278e46ef553cc3176963ed0d52077cdd.jpg",
+      "title": "Elegant Black Watch",
+      "subtitle": "Discover timeless elegance.",
+      "page": DetailPage(title: "Page 1"),
+    },
+    {
+      "image":
+          "https://i.pinimg.com/736x/73/8c/66/738c66a1e117f638020803ff8c6a6575.jpg",
+      "title": "Luxury Silver Watch",
+      "subtitle": "Refined style for modern times.",
+      "page": DetailPage(title: "Page 2"),
+    },
+    {
+      "image":
+          "https://i.pinimg.com/736x/f1/ca/4a/f1ca4a2b4143a4ce875b20261bc91a4c.jpg",
+      "title": "Classic Leather Watch",
+      "subtitle": "Vintage charm meets innovation.",
+      "page": DetailPage(title: "Page 3"),
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +52,7 @@ class _HomeScreenState extends State<HomeScreen> {
               SizedBox(height: 10),
               CustomSearchBar(),
               SizedBox(height: 20),
-              OffersBanner(),
+              OffersBanner(sliderItems: sliderItems),
               SizedBox(height: SizeManager.getSize(context).height * 0.02),
               Text(
                 AppString.categories,
