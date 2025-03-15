88220e9b54677a801ed27e22d28499c54638fcfa
Your Name <your_email@example.com>
2025-03-16 01:53:44 +0200

    Add FeaturesTitle component for displaying section titles with optional action

---
+++ ecommerce_app/lib/core/components/features_title.dart
@@ -0,0 +1,35 @@
+import 'package:ecommerce_app/core/resources/app_stings.dart';
+import 'package:ecommerce_app/core/styles/padding_manager.dart';
+import 'package:ecommerce_app/core/styles/styles_manager.dart';
+import 'package:flutter/material.dart';
+
+class FeaturesTitle extends StatelessWidget {
+  final String title;
+  final void Function()? onTap;
+  const FeaturesTitle({super.key, required this.title, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: PaddingManager.verticalMain(context),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: StyleManager.textStyle22(context, FontWeight.w700),
+          ),
+          onTap != null
+              ? GestureDetector(
+                onTap: onTap,
+                child: Text(
+                  AppString.seeAll,
+                  style: StyleManager.textStyle14(context, FontWeight.w400),
+                ),
+              )
+              : SizedBox(),
+        ],
+      ),
+    );
+  }
+}
