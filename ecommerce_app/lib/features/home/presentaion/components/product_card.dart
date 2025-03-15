221ec1c4ffd263f2ef3ba1369669f20b0e144d45
Your Name <your_email@example.com>
2025-03-16 01:54:08 +0200

    Add FeaturedProductCard component for displaying product details

---
+++ ecommerce_app/lib/features/home/presentaion/components/product_card.dart
@@ -0,0 +1,31 @@
+import 'package:ecommerce_app/core/styles/size_manager.dart';
+import 'package:ecommerce_app/features/home/presentaion/components/product_components/build_Action_Buttons.dart';
+import 'package:ecommerce_app/features/home/presentaion/components/product_components/build_productInfo.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedProductCard extends StatelessWidget {
+  final Map<String, dynamic> product;
+
+  const FeaturedProductCard({required this.product, Key? key})
+    : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: SizeManager.getSize(context).width * 0.5,
+      child: Column(
+        children: [
+          buildProductInfo(
+            context,
+            productImage: product["image"],
+            title: product["title"],
+            subtitle: product["subtitle"],
+            rating: product["rating"],
+            price: product["price"],
+          ),
+          buildActionButtons(context),
+        ],
+      ),
+    );
+  }
+}
