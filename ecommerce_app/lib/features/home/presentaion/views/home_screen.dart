18554fc5a9f458a47eea4ee33be53233b32dddc0
Your Name <your_email@example.com>
2025-03-16 01:54:32 +0200

    Add Featured Products section to HomeScreen with horizontal list view

--- ecommerce_app/lib/features/home/presentaion/views/home_screen.dart
+++ ecommerce_app/lib/features/home/presentaion/views/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:ecommerce_app/core/components/features_title.dart';
 import 'package:ecommerce_app/core/components/timer.dart';
+import 'package:ecommerce_app/core/resources/app_stings.dart';
 import 'package:ecommerce_app/core/styles/padding_manager.dart';
 import 'package:ecommerce_app/core/styles/size_manager.dart';
 import 'package:ecommerce_app/features/home/data/data.dart';
@@ -6,6 +8,7 @@ import 'package:ecommerce_app/features/home/presentaion/components/categories_wi
 import 'package:ecommerce_app/features/home/presentaion/components/custom_search_bar.dart';
 import 'package:ecommerce_app/features/home/presentaion/components/custom_user_bar.dart';
 import 'package:ecommerce_app/features/home/presentaion/components/offers_banner.dart';
+import 'package:ecommerce_app/features/home/presentaion/components/product_card.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -19,26 +22,45 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Padding(
-          padding: PaddingManager.main(context),
-          child: CustomScrollView(
-            slivers: [
-              SliverList(
+        child: CustomScrollView(
+          slivers: [
+            SliverPadding(
+              padding: PaddingManager.main(context),
+              sliver: SliverList(
                 delegate: SliverChildListDelegate([
                   CustomUserBar(),
-                  SizedBox(height: 10),
                   CustomSearchBar(),
-                  SizedBox(height: 20),
-                  OffersBanner(sliderItems: HomeData.sliderItems),
                   SizedBox(height: SizeManager.getSize(context).height * 0.02),
+                  OffersBanner(sliderItems: HomeData.sliderItems),
+                  FeaturesTitle(title: AppString.categories, onTap: () {}),
                   Categories(categoriesList: HomeData.categories),
                   SizedBox(height: SizeManager.getSize(context).height * 0.02),
                   CountdownTimer(),
+                  FeaturesTitle(title: "Featured Products"),
                 ]),
               ),
-            ],
-          ),
+            ),
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: SizeManager.getSize(context).height * 0.37,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder:
+                      (context, index) => Padding(
+                        padding: EdgeInsets.only(
+                          left: SizeManager.getSize(context).width * 0.05,
+                        ),
+                        child: FeaturedProductCard(
+                          product: HomeData.featuredProducts[index],
+                        ),
+                      ),
+                  itemCount: HomeData.featuredProducts.length,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
