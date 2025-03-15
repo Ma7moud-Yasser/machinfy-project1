54f14131c644f8993a314c876e8c55817e74638c
Your Name <your_email@example.com>
2025-03-16 01:54:23 +0200

    Add buildProductPriceAndRating widget for displaying product price and rating

---
+++ ecommerce_app/lib/features/home/presentaion/components/product_components/product_price_rating.dart
@@ -0,0 +1,36 @@
+import 'package:ecommerce_app/core/styles/assets_manager.dart';
+import 'package:ecommerce_app/core/styles/color_manager.dart';
+import 'package:ecommerce_app/core/styles/styles_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+Widget buildProductPriceAndRating(
+  BuildContext context, {
+  required String price,
+  required String rating,
+}) {
+  return Row(
+    children: [
+      Text(
+        "$price EGP",
+        style: StyleManager.textStyle16(
+          context,
+          FontWeight.w700,
+        ).copyWith(color: AppColor.secondary),
+      ),
+      const Spacer(),
+      Row(
+        children: [
+          Text(
+            rating,
+            style: StyleManager.textStyle14(
+              context,
+              FontWeight.w500,
+            ).copyWith(color: AppColor.primary),
+          ),
+          SvgPicture.asset(IconsAssets.starIcon),
+        ],
+      ),
+    ],
+  );
+}
