a6302fac23cbfefc0110e6dd64717a9a0921e234
Your Name <your_email@example.com>
2025-03-16 03:57:46 +0200

    Remove unused import for most viewed product card in home screen

--- ecommerce_app/lib/features/home/presentaion/views/home_screen.dart
+++ ecommerce_app/lib/features/home/presentaion/views/home_screen.dart
@@ -8,7 +8,6 @@ import 'package:ecommerce_app/features/home/presentaion/components/categories_wi
 import 'package:ecommerce_app/features/home/presentaion/components/custom_search_bar.dart';
 import 'package:ecommerce_app/features/home/presentaion/components/custom_user_bar.dart';
 import 'package:ecommerce_app/features/home/presentaion/components/most_viewed_component/most_view_builder.dart';
-import 'package:ecommerce_app/features/home/presentaion/components/most_viewed_component/most_viewed_product_card.dart';
 import 'package:ecommerce_app/features/home/presentaion/components/offers_banner.dart';
 import 'package:ecommerce_app/features/home/presentaion/components/product_view_builder.dart';
 import 'package:flutter/material.dart';
