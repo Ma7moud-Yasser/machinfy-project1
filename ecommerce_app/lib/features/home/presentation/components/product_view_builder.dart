86ceb377e8e28d7be35934eacbd051c237dfd316
Your Name <your_email@example.com>
2025-03-19 01:26:33 +0200

    Integrate FeaturedProductsCubit into ProductListViewBuilder for state management

--- ecommerce_app/lib/features/home/presentation/components/product_view_builder.dart
+++ ecommerce_app/lib/features/home/presentation/components/product_view_builder.dart
@@ -1,6 +1,8 @@
 import 'package:ecommerce_app/core/styles/size_manager.dart';
 import 'package:ecommerce_app/features/home/presentation/components/featured_product_card.dart';
+import 'package:ecommerce_app/features/home/presentation/controllers/featured_products_cubit/featured_products_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProductListViewBuilder extends StatelessWidget {
   final List<Map<String, dynamic>> productList;
@@ -8,19 +10,26 @@ class ProductListViewBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: SizeManager.getSize(context).height * 0.35,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder:
-            (context, index) => Padding(
-              padding: EdgeInsets.only(
-                left: SizeManager.getSize(context).width * 0.05,
-              ),
-              child: FeaturedProductCard(product: productList[index]),
-            ),
-        itemCount: productList.length,
-      ),
+    return BlocConsumer<FeaturedProductsCubit, FeaturedProductsState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        return SizedBox(
+          height: SizeManager.getSize(context).height * 0.35,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemBuilder:
+                (context, index) => Padding(
+                  padding: EdgeInsets.only(
+                    left: SizeManager.getSize(context).width * 0.05,
+                  ),
+                  child: FeaturedProductCard(product: productList[index]),
+                ),
+            itemCount: productList.length,
+          ),
+        );
+      },
     );
   }
 }
